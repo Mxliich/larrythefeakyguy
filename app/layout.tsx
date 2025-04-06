def8d5cf3b0b706b5b8c280d7a2509fc577683e7
Mxliich <162749408+Mxliich@users.noreply.github.com>
2025-04-06 13:07:45 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
-import type React from "react"
-import "/styles/globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
+import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Missed Call From Larry",
-  description: "Take a call with Larry, the spooky yet silly cat!",
-    generator: 'v0.dev'
+  description: "A creepy cat calling experience",
 }
 
 export default function RootLayout({
@@ -18,13 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
-      </body>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
